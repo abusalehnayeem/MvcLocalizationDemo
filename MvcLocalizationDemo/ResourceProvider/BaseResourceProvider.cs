9e914e9bcc2e1f50020bd885efad6a39133d6654
Abusaleh Nayeem <nayeemmbstu@gmail.com>
2016-12-15 01:37:17 +0600

    Added Resource Provider Class for supporting globalization & localization.

---
+++ MvcLocalizationDemo/ResourceProvider/BaseResourceProvider.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Resources;
+using System.Web;
+using System.Web.Compilation;
+
+namespace MvcLocalizationDemo.ResourceProvider
+{
+    public abstract class BaseResourceProvider: IResourceProvider
+    {
+        public object GetObject(string resourceKey, CultureInfo culture)
+        {
+            return null;
+        }
+
+        public IResourceReader ResourceReader { get; }
+    }
+}
