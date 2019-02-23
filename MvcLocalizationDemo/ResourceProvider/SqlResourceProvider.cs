876f68414d2a6d4680e158700741b45339fde9df
Abusaleh Md Nayeem <nayeemmbstu@gmail.com>
2019-02-23 23:51:49 +0600

    Update Nuget

--- MvcLocalizationDemo/ResourceProvider/SqlResourceProvider.cs
+++ MvcLocalizationDemo/ResourceProvider/SqlResourceProvider.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 using System.Resources;
-using System.Web;
 using System.Web.Compilation;
 using MvcLocalizationDemo.Data;
 using MvcLocalizationDemo.DTO;
@@ -11,17 +10,43 @@ using MvcLocalizationDemo.Models;
 
 namespace MvcLocalizationDemo.ResourceProvider
 {
-    public class SqlResourceProvider: IResourceProvider
+    public class SqlResourceProvider : IResourceProvider
     {
-        private readonly ApplicationDbContext _db=new ApplicationDbContext();
-        private readonly IResourceReader _resourceReader;
-        private static Dictionary<string, ResourceEntryDTO> _resources = null;
-        private static object lockResources = new object();
-        protected bool Cache { get; set; } = true;
+        private static Dictionary<string, ResourceEntryDTO> _resources;
+        private static readonly object LockResources = new object();
+        private readonly ApplicationDbContext _db = new ApplicationDbContext();
+        private bool Cache { get; set; } = true;
 
-        protected List<ResourceEntryDTO> ReadResources()
+        public object GetObject(string resourceKey, CultureInfo culture)
         {
-            return _db.ResourceEntry.Select(r => new ResourceEntryDTO()
+            if (string.IsNullOrEmpty(resourceKey) || string.IsNullOrWhiteSpace(resourceKey))
+                throw new ArgumentNullException(nameof(resourceKey));
+            if (culture == null)
+                return null;
+            if (_resources != null)
+                return Cache
+                    ? _resources[$"{resourceKey}.{culture}"].Value
+                    : ReadResource(resourceKey, culture).Value;
+            lock (LockResources)
+            {
+                var dictionary = new Dictionary<string, ResourceEntryDTO>();
+                foreach (var resource in ReadResources())
+                    dictionary.Add(string.Format("{0}.{1}", resource.Name, resource.Culture), resource);
+                _resources = dictionary;
+            }
+            return Cache
+                ? _resources[string.Format("{0}.{1}", resourceKey, culture)].Value
+                : ReadResource(resourceKey, culture).Value;
+        }
+
+        public IResourceReader ResourceReader
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        private IEnumerable<ResourceEntryDTO> ReadResources()
+        {
+            return _db.ResourceEntry.Select(r => new ResourceEntryDTO
             {
                 Culture = r.Culture,
                 Name = r.Name,
@@ -29,36 +54,10 @@ namespace MvcLocalizationDemo.ResourceProvider
             }).ToList();
         }
 
-        protected ResourceEntry ReadResource(string name, CultureInfo culture)
+        private ResourceEntry ReadResource(string name, CultureInfo culture)
         {
             var cultureName = culture.Name;
             return _db.ResourceEntry.FirstOrDefault(r => r.Name == name && r.Culture == cultureName);
-        }
-
-        public object GetObject(string resourceKey, CultureInfo culture)
-        {
-            if (string.IsNullOrEmpty(resourceKey) || string.IsNullOrWhiteSpace(resourceKey))
-            {
-                throw new ArgumentNullException(nameof(resourceKey));
-            }
-            if (culture == null)
-                return null;
-            if (_resources == null)
-            {
-                lock (lockResources)
-                {
-                    var dictionary = new Dictionary<string, ResourceEntryDTO>();
-                    foreach (var resource in ReadResources())
-                        dictionary.Add(string.Format("{0}.{1}", resource.Name, resource.Culture), resource);
-                    _resources = dictionary;
-                }
-            }
-            return Cache ? _resources[string.Format("{0}.{1}", resourceKey,culture)].Value : ReadResource(resourceKey, culture).Value;
-        }
-
-        public IResourceReader ResourceReader
-        {
-            get { return _resourceReader; }
         }
     }
 }
